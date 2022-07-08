6771cbcd7649bd788256c9fdd9d7b0c38dbeb1b3
TheKresh <viktorraboczki@gmail.com>
2022-07-08 17:09:49 +0200

    HomeWork_2
    2022.07.08.

---
+++ Assets/HomeWork2/MyMath.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    /* Nézzetek utána a következõ Mathf függvényeknek és írjátok meg a saját verziótokat belõlük:
+    Max, Min, Clamp, Clamp01, Abs, Sign, Floor, Ceil, Round, 
+    Ne használjatok segítségül Mathf függvéneket, de a sakátjaitokat felhsználhatjátok.
+    Lényeg, hogy gyakoroljátok a függvények írását.
+    Ezek mehetnek egy skriptbe is. */
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
