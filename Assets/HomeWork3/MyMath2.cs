e63a058d4584d3aaa594271adbf772080a21ad8d
TheKresh <viktorraboczki@gmail.com>
2022-07-23 14:38:31 +0200

    HomeWork4

--- Assets/HomeWork3/MyMath2.cs
+++ Assets/HomeWork3/MyMath2.cs
@@ -2,27 +2,27 @@ using UnityEngine;
 
 public class MyMath2 : MonoBehaviour
 {
-    /* Írj függvényt, ami kiszámolja két szám legnagyobb közös osztóját!
-       Írj függvényt, ami kiszámolja két szám legkisebb közös többszörösét! */
+    /* Írj függvényt, ami kiszámolja két szám legnagyobb közös többszörösét!
+       Írj függvényt, ami kiszámolja két szám legkisebb közös osztóját! */
 
     /* [SerializeField] float num1;
     [SerializeField] float num2;
     [Space]
-    [SerializeField] float leastCommonDivider;
-    [SerializeField] float greatestCommonMultiple; // divider-t, multiple-t megcserélni
+    [SerializeField] float leastCommonMultiple;
+    [SerializeField] float greatestCommonDivider; // divider-t, multiple-t megcserélni
 
     void OnValidate()
     {
-        leastCommonDivider = LCD(num1, num2);
-        greatestCommonMultiple = GCM(num1, num2);
+        leastCommonMultiple = LCM(num1, num2);
+        greatestCommonDivider = GCD(num1, num2);
     }
 
-    float LCD(float n1, float n2)
+    float LCM(float n1, float n2)
     {
         
     }
 
-    float GCM(float n1, float n2)
+    float GCD(float n1, float n2)
     {
 
     } */
